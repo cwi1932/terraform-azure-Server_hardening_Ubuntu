bbf315625b8658d0da1bea24aa75838ce8e1bb41
Pramod Sasi <pramodsasi@Pramods-MacBook-Air.local>
2026-09-08 12:08:38 +0530

    Add ansible job to pass terraform outputs and upload inventory artifact

--- Terraform/Terraform/provider.tf
+++ Terraform/Terraform/provider.tf
@@ -1,13 +1,22 @@
 terraform {
+  backend "azurerm" {
+    resource_group_name  = "MyResourceGroup"
+    storage_account_name = "azststest1983"
+    container_name       = "tfstate"
+    key                  = "terraform.tfstate"
+
+    # THIS IS THE CRITICAL FIX FOR THE BACKEND STAGE
+    use_azuread_auth = true
+  }
+
   required_version = ">=1.1.0"
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
       version = "~>4.0"
-
     }
 
-
+    # FIX: These are now properly placed inside required_providers
     random = {
       source  = "hashicorp/random"
       version = "~> 3.0"
@@ -19,23 +28,14 @@ terraform {
     }
   }
 }
-backend "azurerm" {
-    resource_group_name  = "rg-tfstate"
-    storage_account_name = "tfstatepramod23062026"
-    container_name       = "tfstate"
-    key                  = "terraform.tfstate"
 
-    use_azuread_auth = true
-
-  }
-  resource "tls_private_key" "ssh" {
-    algorithm = "RSA"
-    rsa_bits  = 4096
-  }
-
+# Keep your resource declaration here
+resource "tls_private_key" "ssh" {
+  algorithm = "RSA"
+  rsa_bits  = 4096
+}
 
 provider "azurerm" {
   features {}
 
 }
-
--- Terraform/Terraform/variable.tf
+++ Terraform/Terraform/variable.tf
@@ -1,5 +1,12 @@
 variable "Location" {
-    type =string
-    description="This is the region of azuer infrastructure"
+  type        = string
+  description = "This is the region of azuer infrastructure"
 
 }
+
+variable "load_balancer_name" {
+  type        = string
+  description = "This is the name of the load balancer"
+
+}
+
